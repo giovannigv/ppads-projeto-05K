ea92bdae4d802b5be8678d5172087d29d5935bd6
Giovanni Goncalves Vicario <giovanni.goncalves@ibm.com>
2018-03-22 20:26:10 -0300

    It's work!

--- MackFood/src/app/app.module.ts
+++ MackFood/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MenuPage } from '../pages/menu/menu';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MenuPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MenuPage
   ],
   providers: [
     StatusBar,
--- MackFood/src/pages/home/home.ts
+++ MackFood/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MenuPage } from '../menu/menu';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  singInUser(){
+    this.navCtrl.setRoot(MenuPage);
+  }
 }
