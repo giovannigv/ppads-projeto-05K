3e8d399f78d0429e1c54ed1d6dbf04b077dbd4dd
Renato Lourenco da Silva <rlourencos@br.ibm.com>
2018-03-29 18:44:54 -0300

    correção de visual tela de cadastro

--- MackFood/src/pages/menu/menu.module.ts
+++ MackFood/src/pages/menu/menu.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { MenuPage } from './menu';
 
 @NgModule({
-  declarations: [
-    MenuPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(MenuPage),
-  ],
+  declarations: [ ],
+  imports: [ ],
 })
 export class MenuPageModule {}
--- MackFood/src/pages/registrar/registrar.module.ts
+++ MackFood/src/pages/registrar/registrar.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { RegistrarPage } from './registrar';
 
 @NgModule({
-  declarations: [
-    RegistrarPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(RegistrarPage),
-  ],
+  declarations: [  ],
+  imports: [  ],
 })
 export class RegistrarPageModule {}
