7402c7c2d6a95d8f8324105b117b7a2200078110
Giovanni Goncalves Vicario <giovanni.goncalves@ibm.com>
2018-04-20 13:33:13 -0300

    entregavel para o professor pronto

--- MackFood/src/pages/registra-lanchonete/registra-lanchonete.ts
+++ MackFood/src/pages/registra-lanchonete/registra-lanchonete.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CadastroProvider } from '../../providers/cadastro/cadastro';
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -8,15 +11,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegistraLanchonetePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private userCad: FormGroup;
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private cadastroProvider: CadastroProvider,
+    private fb: FormBuilder,
+    private alertCtrl: AlertController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegistraLanchonetePage');
+  ngOnInit() {
+    this.userCad = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      nome: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
   }
 
-  signLanchonete() {
-
+  onSubmit() {
+    console.log(this.userCad.value);
+    this.cadastroProvider.cadastraUser(this.userCad.value).subscribe(
+      res => {
+        console.log(res);
+        setTimeout(() => this.presentAlert('SUCESSO :)', 'Cadastrado com Sucesso!!!'), 100);
+        this.navCtrl.setRoot(HomePage);
+      },
+      err => {
+        console.log(err);
+        this.presentAlert('Ops... Temos um problema', err._body);
+      }
+    )
   }
+  //TODO: Colocar um loading quando estiver mandando a informaçao
 
+  presentAlert(title: string, errorMsg: string) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: errorMsg,
+      buttons: [{
+        text: 'Ok',
+        role: 'Ok'
+      }]
+    });
+    alert.present();
+  }
 }
