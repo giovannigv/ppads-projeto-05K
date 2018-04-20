c77ee97b11276a678ca9cb30cb071aa7e0730109
Renato Lourenco da Silva <rlourencos@br.ibm.com>
2018-04-20 10:42:20 -0300

    Corrigido problema na navegação

--- MackFood/src/pages/registra-lanchonete/registra-lanchonete.ts
+++ MackFood/src/pages/registra-lanchonete/registra-lanchonete.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the RegistraLanchonetePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-registra-lanchonete',
@@ -21,8 +14,9 @@ export class RegistraLanchonetePage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegistraLanchonetePage');
   }
-  signLanchonete(){
-    
+
+  signLanchonete() {
+
   }
 
 }
