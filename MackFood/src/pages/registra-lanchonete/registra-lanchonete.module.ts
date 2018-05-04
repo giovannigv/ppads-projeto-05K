35e561c8f0e58215520da0917bc111b7dfcfd5f1
Renato Lourenco da Silva <rlourencos@br.ibm.com>
2018-05-04 18:51:25 -0300

    alteração da label empresa

--- MackFood/src/pages/registra-lanchonete/registra-lanchonete.module.ts
+++ MackFood/src/pages/registra-lanchonete/registra-lanchonete.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { RegistraLanchonetePage } from './registra-lanchonete';
 
 @NgModule({
-  declarations: [
-    RegistraLanchonetePage,
-  ],
-  imports: [
-    IonicPageModule.forChild(RegistraLanchonetePage),
-  ],
+  declarations: [ ],
+  imports: [ ],
 })
 export class RegistraLanchonetePageModule {}
